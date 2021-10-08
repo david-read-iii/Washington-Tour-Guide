782e8152f059ecf81e8d4d16bd3507fd146fb36a
David <davidread1998@gmail.com>
2021-10-08 18:02:19 -0400

    Made Place.java parcelable and created PlacesListFragment.java
    Made the following changes:
    - Place.java: Implemented the Parcelable interface to make objects of this class parcelable. This allows such objects to be passed as arguments to activity and fragment classes.
    - PlacesListFragment.java: Created a fragment class that shows a ListView of Place objects. Such objects are passed as an argument to the fragment.

--- app/src/main/java/com/davidread/washingtontourguide/Fragments/PlacesListFragment.java
+++ app/src/main/java/com/davidread/washingtontourguide/Fragments/PlacesListFragment.java
@@ -1,4 +1,74 @@
 package com.davidread.washingtontourguide.Fragments;
 
-public class PlacesListFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.davidread.washingtontourguide.Adapters.PlaceAdapter;
+import com.davidread.washingtontourguide.Models.Place;
+import com.davidread.washingtontourguide.R;
+
+import java.util.ArrayList;
+
+/**
+ * {@link PlacesListFragment} represents a {@link View} object with a {@link ListView}. The
+ * {@link ListView} displays the {@link ArrayList} of {@link Place} objects passed into the
+ * fragment as an argument.
+ */
+public class PlacesListFragment extends Fragment {
+
+    // Member variable.
+    private ArrayList<Place> places;
+
+    /**
+     * Returns a {@link PlacesListFragment} object with the specified {@link ArrayList} object
+     * passed as an argument.
+     *
+     * @param places An {@link ArrayList} of {@link Place} objects to be displayed in the fragment's
+     *               {@link android.widget.ListView}.
+     * @return A {@link PlacesListFragment} with arguments on it.
+     */
+    public static PlacesListFragment newInstance(ArrayList<Place> places) {
+        PlacesListFragment fragment = new PlacesListFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList("places", places);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * During onCreate() phase, store member variables passed to this fragment as arguments.
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        places = getArguments().getParcelableArrayList("places");
+    }
+
+    /**
+     * Returns a {@link View} object for this fragment.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Inflate the fragment's view.
+        View view = inflater.inflate(R.layout.fragment_places_list, container, false);
+
+        // Create adapter to adapt objects in the ArrayList into views for the ListView.
+        PlaceAdapter adapter = new PlaceAdapter(getContext(), places);
+
+        // Attach adapter to the ListView in the fragment's view.
+        ListView listView = view.findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        // Return the fragment's view.
+        return view;
+    }
 }
--- app/src/main/java/com/davidread/washingtontourguide/Models/Place.java
+++ app/src/main/java/com/davidread/washingtontourguide/Models/Place.java
@@ -1,17 +1,34 @@
 package com.davidread.washingtontourguide.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * {@link Place} is a model class that represents a place of interest in Washington. Each
  * {@link Place} object has a name, address, and description.
  */
-public class Place {
+public class Place implements Parcelable {
 
+    // Object that generates instances of this class from a Parcel object.
+    public static final Creator<Place> CREATOR = new Creator<Place>() {
+        @Override
+        public Place createFromParcel(Parcel in) {
+            return new Place(in);
+        }
+
+        @Override
+        public Place[] newArray(int size) {
+            return new Place[size];
+        }
+    };
+
+    // Member variables.
     private String name;
     private String address;
     private String description;
 
     /**
-     * Constructs a new {@link Place} object.
+     * Constructs a new {@link Place} object given its member variables.
      *
      * @param name        Name of the place.
      * @param address     Street address of the place.
@@ -21,6 +38,18 @@ public class Place {
         this.name = name;
         this.address = address;
         this.description = description;
+    }
+
+    /**
+     * Construct a new {@link Place} object given a {@link Parcel} object.
+     *
+     * @param in The {@link Parcel} object that contains the member variables of the {@link Place}
+     *           object.
+     */
+    protected Place(Parcel in) {
+        name = in.readString();
+        address = in.readString();
+        description = in.readString();
     }
 
     /**
@@ -48,5 +77,28 @@ public class Place {
      */
     public String getDescription() {
         return description;
+    }
+
+    /**
+     * Returns an int describing the type of objects contained in the {@link Parcelable} instance.
+     *
+     * @return An int describing the type of objects contained in the {@link Parcelable} instance.
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Converts the member variables of this object into a {@link Parcel} object.
+     *
+     * @param dest  The {@link Parcel} object where the member variables will be stored.
+     * @param flags Additional flags.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(address);
+        dest.writeString(description);
     }
 }
